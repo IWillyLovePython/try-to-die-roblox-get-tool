7528cf7a4726bb2c805279d8f7aee56334a117fd
IWillyLovePython <104280094+IWillyLovePython@users.noreply.github.com>
2022-08-24 22:08:08 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,36 @@
+local toolSpawn = {game:GetService("Workspace").Levels.Level22.Bombispenser, game:GetService("Workspace").Levels.Level21.RocketLauncherDispenser, game:GetService("Workspace").Levels.Level23.SlingShotDispenser}
+local keybind = Enum.KeyCode.T
+
+function getTools()
+    for _, v in pairs(toolSpawn) do
+        for _, j in pairs(v:GetChildren()) do
+            if v:IsA("Tool") then
+                repeat wait() until v:FindFirstChild("Handle")
+                repeat wait() until game.Players.LocalPlayer.Character.HumanoidRootPart
+                v:FindFirstChild("Handle").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            end
+        end
+    end
+end
+
+function waitTools()
+    for _, v in pairs(toolSpawn) do
+        v.ChildAdded:Connect(function(child) 
+            if child:IsA("Tool") then
+                repeat wait() until child:FindFirstChild("Handle")
+                repeat wait() until game.Players.LocalPlayer.Character.HumanoidRootPart
+                child:FindFirstChild("Handle").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+             end
+        end)
+    end
+end
+
+waitTools()
+getTools()
+
+game:GetService("UserInputService").InputBegan:Connect(function(InputObject, GameProcessedEvent)
+    if GameProcessedEvent then return end
+    if InputObject.KeyCode == keybind then
+        getTools()
+    end
+end)
